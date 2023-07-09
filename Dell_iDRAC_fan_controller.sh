620101f5a3219bf06b0b1c52fcb6516e2e84fbb3
Alvin Cherk <alvin@cherk.org>
2023-07-09 18:18:48 +1000

    Disable default on exit

--- Dell_iDRAC_fan_controller.sh
+++ Dell_iDRAC_fan_controller.sh
@@ -83,10 +83,11 @@ function disable_third_party_PCIe_card_Dell_default_cooling_response () {
 # }
 
 # Prepare traps in case of container exit
-function gracefull_exit () {
-  apply_Dell_fan_control_profile
-  enable_third_party_PCIe_card_Dell_default_cooling_response
-  echo "/!\ WARNING /!\ Container stopped, Dell default dynamic fan control profile applied for safety."
+function gracefull_exit() {
+  # apply_Dell_fan_control_profile
+  # enable_third_party_PCIe_card_Dell_default_cooling_response
+  # echo "/!\ WARNING /!\ Container stopped, Dell default dynamic fan control profile applied for safety."
+  echo "/!\ WARNING /!\ Container stopped"
   exit 0
 }
 
